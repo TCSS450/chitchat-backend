8e1e0c7988dc77db9080c23dd54b00dcbb509bf7
Hari Kuduva <harikuduva7@gmail.com>
2018-10-30 00:34:53 -0700

    login implented I think 2

--- routes/login.js
+++ routes/login.js
@@ -19,9 +19,12 @@ router.post('/', (req, res) => {
     if(email && theirPw) {
         db.any("SELECT Password, Salt FROM Members WHERE Email=$1", [email])
             .then(row => { // if query execution is valid
+                console.log("Entered the Query!");
                 if (row.length === 0) { // no rows returned
                     toSend.status = 2;
                 } else  if (row.length === 1) { // single row returned
+                    console.log("HERE 2");
+                    //toSend.status = row;
                     let salt = row['salt'];
                     //Retrieve our copy of the password
                     let ourSaltedHash = row['password']; 
@@ -29,9 +32,13 @@ router.post('/', (req, res) => {
                     let theirSaltedHash = getHash(theirPw, salt); 
                     //Did our salted hash match their salted hash?
                     const wasCorrectPw = ourSaltedHash === theirSaltedHash; 
+                    console.log(wasCorrectPw);
                     toSend.status = (wasCorrectPw) ? 1 : 3;
                 }
-            });
+            })
+            .catch(() => {
+                toSend.status = 100;
+            })
     } 
     res.send(toSend);
 });
