839e8b97ccd21aa9bed16da16fd6eef55801799a
Hari Kuduva <harikuduva7@gmail.com>
2018-10-30 01:06:24 -0700

    Login fully working

--- routes/login.js
+++ routes/login.js
@@ -3,43 +3,34 @@ const express = require('express');
 
 //Create connection to Heroku Database
 let db = require('../utilities/utils').db;
-
 let getHash = require('../utilities/utils').getHash;
-
 var router = express.Router();
-
 const bodyParser = require("body-parser");
+const crypto = require("crypto");
 //This allows parsing of the body of POST requests, that are encoded in JSON
 router.use(bodyParser.json());
 
 router.post('/', (req, res) => {
     let email = req.body['email'];
     let theirPw = req.body['password'];
-    const toSend = {"status": 4};
     if(email && theirPw) {
         db.any("SELECT Password, Salt FROM Members WHERE Email=$1", [email])
             .then(row => { // if query execution is valid
-                console.log("Entered the Query!");
                 if (row.length === 0) { // no rows returned
-                    toSend.status = 2;
+                    res.send({"status" : 2});
                 } else  if (row.length === 1) { // single row returned
-                    console.log("HERE 2");
-                    //toSend.status = row;
-                    let salt = row['salt'];
-                    //Retrieve our copy of the password
-                    let ourSaltedHash = row['password']; 
-                    //Combined their password with our salt, then hash
+                    let salt = row[0].salt;
+                    let ourSaltedHash = row[0].password; 
                     let theirSaltedHash = getHash(theirPw, salt); 
-                    //Did our salted hash match their salted hash?
                     const wasCorrectPw = ourSaltedHash === theirSaltedHash; 
-                    console.log(wasCorrectPw);
-                    toSend.status = (wasCorrectPw) ? 1 : 3;
+                    res.send({"status": (wasCorrectPw) ? 1 : 3});
                 }
             })
             .catch(() => {
-                toSend.status = 100;
+                res.send({"status": 4});
             })
-    } 
-    res.send(toSend);
+    } else {
+        res.send({"status": 4});
+    }
 });
 module.exports = router;
