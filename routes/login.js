f64a366ecc45c29ada7031ca6b9d05791143f7e7
Fasil A <ethio@uw.edu>
2018-11-07 20:09:24 -0800

    firebase token fix2

--- routes/login.js
+++ routes/login.js
@@ -16,36 +16,6 @@ router.use(bodyParser.json());
 		4- Email/NN exists, but user is still not verified
         5- Incorrect Input to endpoint / any other error */
         
-router.post('/', (req, res) => {
-    const emailNN = req.body['email'];
-    const theirPw = req.body['password'];
-    if (emailNN && theirPw) {
-        db.any("SELECT Password, Salt, is_verified FROM Members WHERE Email=$1 OR Nickname=$1", [emailNN])
-            .then(row => { //if query execution is valid
-                if (row.length === 0) { // Email or NN DNE in DB
-                    res.send({"status" : 2});
-                } else if (row.length === 1) { // single row returned
-                    if (row[0].is_verified) { // if that account is verified
-                        let salt = row[0].salt;
-                        let ourSaltedHash = row[0].password; 
-                        let theirSaltedHash = getHash(theirPw, salt); 
-                        const wasCorrectPw = ourSaltedHash === theirSaltedHash; 
-                        console.log(theirPw);
-                        res.send({"status": (wasCorrectPw) ? 1 : 3});
-                    } else { // Email or NN exists in DB but unverified account
-                        console.log(row[0].is_verified);
-                        console.log(row);
-                        res.send({"status" : 4});
-                    }
-                }
-            })
-            .catch(() => {
-                res.send({"status" : 5});
-            });
-    } else {
-        res.send({"status" : 5});
-    }
-});
 //firebase token storage
 router.post('/with_token', (req, res) => {
     let email = req.body['email'];
