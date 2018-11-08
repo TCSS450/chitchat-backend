5e17d43ae28016468fdaa1ed9fc5cc84a5bb6ab3
Fasil A <ethio@uw.edu>
2018-11-07 22:42:34 -0800

    firebase token!!!

--- utilities/firebase_services.js
+++ utilities/firebase_services.js
@@ -41,7 +41,7 @@ function sendToIndividual(token, msg, from) {
                 title: 'New Message from '.concat(from),
                 body: msg,
                 color: "#0000FF",
-                //icon: '@drawable/ic_notification_phish'
+                icon: '@mipmap/ic_launcher_foreground'
             },
             data: {
                 "type": "contacrt",
