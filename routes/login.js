64da9e26ec30df7babd9aa8d7fcecdaaa6a2780b
Fasil A <ethio@uw.edu>
2018-11-10 01:57:03 -0800

    login token test

--- routes/login.js
+++ routes/login.js
@@ -19,7 +19,7 @@ router.use(bodyParser.json());
         
 router.post('/with_token', (req, res) => {
     const emailNN = req.body['email'];
-    let token = req.body['token'];
+    const token = req.body['token'];
     const theirPw = req.body['password'];
     if (emailNN && theirPw && token) {
         db.any("SELECT MemberID, Password, Salt, is_verified FROM Members WHERE Email=$1 OR Nickname=$1", [emailNN])
@@ -32,6 +32,8 @@ router.post('/with_token', (req, res) => {
                         let ourSaltedHash = row[0].password; 
                         let theirSaltedHash = getHash(theirPw, salt); 
                         const wasCorrectPw = ourSaltedHash === theirSaltedHash;
+                        console.log(theirPw);
+                        res.send({"status": (wasCorrectPw) ? 1 : 3});
                         if (wasCorrectPw) {
                             //password and email match. Save the current FB Token
                             let id = row['memberid'];
@@ -52,9 +54,7 @@ router.post('/with_token', (req, res) => {
                                         message: err
                                     });
                                 })
-                        } 
-                        console.log(theirPw);
-                        res.send({"status": (wasCorrectPw) ? 1 : 3});
+                        }
                     } else { // Email or NN exists in DB but unverified account
                         console.log(row[0].is_verified);
                         console.log(row);
@@ -105,3 +105,59 @@ router.post('/', (req, res) => {
 
 
 module.exports = router;
+
+
+
+/*
+
+//express is the framework we're going to use to handle requests
+const express = require('express');
+//Create connection to Heroku Database
+let db = require('../utilities/utils').db;
+let getHash = require('../utilities/utils').getHash;
+var router = express.Router();
+const bodyParser = require("body-parser");
+//This allows parsing of the body of POST requests, that are encoded in JSON
+router.use(bodyParser.json());
+
+
+
+router.post('/', (req, res) => {
+    let email = req.body['email'];
+    let theirPw = req.body['password'];
+    let wasSuccessful = false;
+    if (email && theirPw) {
+        //Using the 'one' method means that only one row should be returned
+        db.one('SELECT Password, Salt FROM Members WHERE Email=$1', [email])
+            //If successful, run function passed into .then()
+            .then(row => {
+                let salt = row['salt'];
+                //Retrieve our copy of the password
+                let ourSaltedHash = row['password'];
+
+                //Combined their password with our salt, then hash
+                let theirSaltedHash = getHash(theirPw, salt);
+                //Did our salted hash match their salted hash?
+                let wasCorrectPw = ourSaltedHash === theirSaltedHash;
+                //Send whether they had the correct password or not
+                res.send({
+                    success: wasCorrectPw
+                });
+            })
+            //More than one row shouldn't be found, since table has constraint on it
+            .catch((err) => {
+                //If anything happened, it wasn't successful
+                res.send({
+                    success: false,
+                    message: err
+                });
+            });
+    } else {
+        res.send({
+            success: false,
+            message: 'missing credentials'
+        });
+    }
+});
+module.exports = router;
+*/
