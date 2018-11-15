ec545d0f776d7eabc0967d3d4bf445f0b94c7fd8
Hari Kuduva <harikuduva7@gmail.com>
2018-11-14 19:12:25 -0800

    fix

--- routes/login.js
+++ routes/login.js
@@ -22,11 +22,11 @@ router.post('/with_token', (req, res) => {
     const token = req.body['token'];
     const theirPw = req.body['password'];
     if (emailNN && theirPw && token) {
-        db.any("SELECT MemberID, Password, Salt, is_verified FROM Members WHERE Email=$1 OR Nickname=$1", [emailNN])
+        db.any("SELECT MemberID, Password, Salt, nickname, is_verified FROM Members WHERE Email=$1 OR Nickname=$1", [emailNN])
             .then(row => { //if query execution is valid
                 if (row.length === 0) { // Email or NN DNE in DB
                     res.send({"status" : 2});
-                } else if (row.length === 1) { // single row returned
+                } else if (row.length === 1) { // single row returßned
                     if (row[0].is_verified) { // if that account is verified
                         let salt = row[0].salt;
                         let ourSaltedHash = row[0].password; 
@@ -37,14 +37,16 @@ router.post('/with_token', (req, res) => {
                         
                         if (wasCorrectPw) {    
                             //password and email match. Save the current FB Token
-                            console.log(row[0].memberid);
                             let id = row[0].memberid;
                             let nickname = row[0].nickname;
-                            console.log(id);
                             let params = [id, token];
+                            console.log(row);
+
                             db.any('INSERT INTO FCM_Token (memberid, token) VALUES ($1, $2) ON CONFLICT (memberid) DO UPDATE SET token = $2; ', params)
                                 .then(row => {
                                     console.log("Here");
+                                    console.log(row);
+                                    console.log(nickname);
                                     res.send({
                                         //success: true,
                                         "status": 1,
