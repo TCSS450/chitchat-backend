d40acc502e80e3c41dd1a3850060d34929695e74
Fasil A <ethio@uw.edu>
2018-11-15 18:29:04 -0800

    token error check

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use('/accept-friend-request', require('./routes/accept-friend-request'));
 app.use('/reject-friend-request', require('./routes/reject-friend-request'));
 app.use('/view-request-sent-to-me', require('./routes/view-request-sent-to-me'));
 app.use('/view-request-I-sent', require('./routes/view-requests-I-sent'));
+app.use('/token-test', require('./routes/token-test.js'));
 
 
 
