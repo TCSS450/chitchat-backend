e864cb2c9154f15a28507df6481d51630534614b
Fasil A <ethio@uw.edu>
2018-11-17 16:09:35 -0800

    past chat test

--- routes/messaging.js
+++ routes/messaging.js
@@ -60,8 +60,9 @@ router.post("/getAll", (req, res) => {
     let query = `SELECT Members.Email, Messages.Message, to_char(Messages.Timestamp AT TIME ZONE 'PDT', 'YYYY-MM-DD HH24:MI:SS.US' ) AS Timestamp FROM Messages INNER JOIN Members ON Messages.MemberId=Members.MemberId WHERE ChatId=$1 ORDER BY Timestamp DESC`
     db.manyOrNone(query, [chatId])
         .then((rows) => {
+            let rowEmail = rows[0].email;
             res.send({
-                messages: rows[0].email + ":" + rows[0].message
+                messages: rows[0++].email + ":" + rows[0++].message 
             })
             console.log(rows.email);
         }).catch((err) => {
