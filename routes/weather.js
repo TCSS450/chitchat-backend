0b9a30cb323873b7050003189266191b87179bb7
Josh Atherton <joshua9@uw.edu>
2018-11-20 20:30:08 -0800

    simplified weather endpoint;

--- routes/weather.js
+++ routes/weather.js
@@ -13,44 +13,16 @@ router.use(bodyParser.json());
 /**
  * Get the weather for the past 10 days.
  * app.../weather
- * input: {lat: lattitude, lon: longitude}
+ * input: {lat: lattitude, lon: longitude, days: n} where 0 < n < 17
  * output: {weather: [data for n days of weather], success: boolean}
  */
 const days = 10;
 router.get('/', (req, res) => {
     const lat = req.body['lat'];
     const lon = req.body['lon'];
-    if (lat && lon) {
+    const days = req.body['days'];
+    if (lat && lon && days) {
         let url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&units=I&days=${days}&key=${API_KEY}`;
-
-        //When this web service gets a request, make a request to the Weather Web service
-        request(url, function (error, response, body) {
-            if (error) {
-                res.send(error);
-            } else {
-                // parse out the weather data for today
-                let weather = JSON.parse(body);
-                res.send({"weather":weather.data, "success": true});
-            }
-        }); 
-    } else { // did not supply right parameters
-        res.send({"success": false});
-    }
-});
-
-/**
- * Get the weather for one day.
- * app.../weather/today
- * input: {lat: lattitude, lon: longitude}
- * output: {weather: [data for todays weather], success: boolean}
- */
-router.get('/today', (req, res) => {
-    const lat = req.body['lat'];
-    const lon = req.body['lon'];
-
-    if (lat && lon) {
-
-        let url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&units=I&days=1&key=${API_KEY}`;
 
         //When this web service gets a request, make a request to the Weather Web service
         request(url, function (error, response, body) {
