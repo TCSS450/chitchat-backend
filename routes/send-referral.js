79c38ec6799b9033ceabb0377ad3b6bdb4d32dfb
Hari Kuduva <harikuduva7@gmail.com>
2018-11-30 21:41:10 -0800

    changes in refferal

--- routes/send-referral.js
+++ routes/send-referral.js
@@ -8,18 +8,26 @@ router.post('/', (req, res) => {
     res.type("application/json");
     const email = req.body['email'];
     const memberid = req.body['memberid'];
-    const defaultReturn = {"status": 3};
+    const defaultReturn = {"status": 4};
     if (email && memberid) {
         db.any("SELECT firstname, lastname FROM members WHERE memberid = $1", [memberid])
             .then(rows => {
                 if (rows.length === 1) {
-                    db.any("SELECT email FROM members WHERE email = $1", [email])
+                    db.any("SELECT email, is_verified FROM members WHERE email = $1", [email])
                         .then(secRows => {
                             if (secRows.length === 0) {
                                 utility.sendReferalEmail(email, rows[0].firstname + ' ' + rows[0].lastname);
                                 res.send({"status": 1});
                             } else {
-                                res.send({"status": 2});
+                                if (secRows.length === 1) {
+                                    if (secRows[0].is_verified) {
+                                        res.send({"status": 2});
+                                    } else {
+                                        res.send({"status": 3});
+                                    }
+                                } else {
+                                    res.send(defaultReturn);
+                                }
                             }
                         }).catch(() => {res.send(defaultReturn)})
                 } else {
