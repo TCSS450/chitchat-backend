c623f4fe5c608625899b0918c9c3b30f846fc3be
Hari Kuduva <harikuduva7@gmail.com>
2018-11-30 21:35:16 -0800

    changes with login

--- routes/login.js
+++ routes/login.js
@@ -13,7 +13,7 @@ router.use(bodyParser.json());
 	    1- Successful Login
 		2- Email/NN doesn’t exist in DB (prompt user to register)
 		3- Email/NN exists in DB, but password was incorrect (tell user to re-enter pass)
-		4- Email/NN exists, but user is still not verified
+        4- Email/NN exists, but user is still not verified
         5- Incorrect Input to endpoint / any other error */
 
         
@@ -22,7 +22,7 @@ router.post('/with_token', (req, res) => {
     const token = req.body['token'];
     const theirPw = req.body['password'];
     if (emailNN && theirPw && token) {
-        db.any("SELECT MemberID, Password, Salt, nickname, is_verified FROM Members WHERE Email=$1 OR Nickname=$1", [emailNN])
+        db.any("SELECT MemberID, email, firstname, lastname, phone_number, display_type, Password, Salt, nickname, is_verified FROM Members WHERE Email=$1 OR Nickname=$1", [emailNN])
             .then(row => { //if query execution is valid
                 if (row.length === 0) { // Email or NN DNE in DB
                     res.send({"status" : 2});
@@ -31,64 +31,43 @@ router.post('/with_token', (req, res) => {
                         let salt = row[0].salt;
                         let ourSaltedHash = row[0].password; 
                         let theirSaltedHash = getHash(theirPw, salt); 
-                        const wasCorrectPw = ourSaltedHash === theirSaltedHash;
-                        console.log(theirPw);
-                        //res.send({"status": (wasCorrectPw) ? 1 : 3, "memberId": row[0].memberid});
-                        
+                        const wasCorrectPw = ourSaltedHash === theirSaltedHash;                        
                         if (wasCorrectPw) {    
                             //password and email match. Save the current FB Token
                             let id = row[0].memberid;
-                            let nickname = row[0].nickname;
                             let params = [id, token];
                             console.log(row);
 
                             db.manyOrNone('INSERT INTO FCM_Token (memberId, token) VALUES ($1, $2) ON CONFLICT (memberId) DO UPDATE SET token = $2; ', params)
-                                .then(row => {
-                                    console.log("Here");
-                                    console.log(row);
-                                    console.log(nickname);
+                                .then(() => {
                                     res.send({
-                                        //success: true,
                                         "status": 1,
-                                        "memberId": id,
-                                        "nickname": nickname
-                                        //message: "Token Saved"
+                                        "memberProfile": {
+                                            "memberid": row[0].memberid,
+                                            "email": row[0].email,
+                                            "firstname": row[0].firstname,
+                                            "lastname": row[0].lastname,
+                                            "nickname": row[0].nickname,
+                                            "display_type": row[0].display_type,
+                                            "phone_number": row[0].phone_number
+                                        }
                                     });
                                 })
                                 .catch(err => {
-                                    console.log("Here2");
-                                    //console.log("failed on insert");
-                                    console.log(err);
-                                    //If anything happened, it wasn't successful
                                     res.send({
                                         success: false,
                                         message: err,
                                     });
                                 })
-                        } else {
+                        } else { // incorrect password
                             res.send({
                                 "status": 3,
                                 "memberId": row[0].memberid
                             })
-                            /*
-                            .catch(err => {
-                                console.log("failed on insert");
-                                console.log(err);
-                                //If anything happened, it wasn't successful
-                                res.send({
-                                    success: false,
-                                    message: err,    
-                                    "status": "Error in option 2",                             
-                                });
-                            })
-                            */
                         }  
-                    } /*else { // Email or NN exists in DB but unverified account
-                        console.log(row[0].is_verified);
-                        console.log(row);
-                        res.send({"status" : 3, "memberId": row[0].memberid});
-                    }*/
-                    
+                    } else { // user not verified
+                        res.send({"status": 4});
+                    }
                 }
             })
             .catch(() => {
@@ -99,7 +78,7 @@ router.post('/with_token', (req, res) => {
     }
 });
 
-
+/*
 router.post('/', (req, res) => {
     const emailNN = req.body['email'];
     const theirPw = req.body['password'];
@@ -130,7 +109,7 @@ router.post('/', (req, res) => {
     } else {
         res.send({"status" : 5});
     }
-});
+});*/
 
 
 module.exports = router;
